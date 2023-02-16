14d81f67504055ae0d88557bcccf328ce821af67
Md. Sakil Islam <sakilislam1080@gmail.com>
2023-02-16 12:50:57 +0600

    text modification.

--- src/pages/Manager.jsx
+++ src/pages/Manager.jsx
@@ -6,7 +6,7 @@ import { Header } from '../components';
 const Manager = () => {
   return (
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
-      <Header category="Page" title="Task Manager" />
+      <Header category="App" title="Task Manager" />
       <KanbanComponent
         id='kanban'
         dataSource={kanbanData}
